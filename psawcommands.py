bb91de68b560323038f72eab5509f511ab28346d
Sidharth Kumar <skumar@Cassiopeia.local>
2020-06-03 00:58:45 -0700

    finished cli

--- psawcommands.py
+++ psawcommands.py
@@ -4,11 +4,29 @@ import json
 import csv
 import time
 import datetime
+import praw
+from prawcore import NotFound
 
 
-def getPushshiftData(query, end, start, sub):
-    url = 'https://api.pushshift.io/reddit/search/submission/?title=' + str(query) + '&size=1000&end=' + str(
-        end) + '&start=' + str(start) + '&subreddit=' + str(sub)
+
+
+
+reddit = praw.Reddit(client_id='2ICu9olD83K6OQ',
+                     client_secret='6m4czcAQ7-XrvIyzkiwW0nkY-5Q',
+                     user_agent='my user agent')
+
+
+
+def sub_exists(sub):
+    exists = True
+    try:
+        reddit.subreddits.search_by_name(sub, exact=True)
+    except NotFound:
+        exists = False
+    return exists
+
+def getPushshiftData(query, after, before, sub):
+    url = 'https://api.pushshift.io/reddit/search/submission/?title='+str(query)+'&size=1000&after='+str(after)+'&before='+str(before)+'&subreddit='+str(sub)
     print(url)
     r = requests.get(url)
     data = json.loads(r.text)
@@ -25,7 +43,10 @@ def getCommentData(id, limit):
         collected.append(comment['body'])
     return collected
 
+
     return data['data']
+
+
 
 
 def collectSubData(subm, limit):
@@ -46,19 +67,20 @@ def collectSubData(subm, limit):
     subData.append((sub_id, title, url, author, score, created, numComms, permalink, flair, comments))
     subStats[sub_id] = subData
 
+if reddit.read_only:
+    print("reddit has loaded, welcome to reddit-downloader")
 
-
-end = input()
 temp = False
-if end.isnumeric() and len(end) == 10 :
-    temp = True
+print("Enter the name of the subreddit that you would like to search from :")
+print("eg: for r/askreddit you would enter \"askreddit\"")
+t = input()
+temp = sub_exists(t)
+while temp is False:
+    print("such a subreddit does not exist! try again: ")
+    t = input()
+    temp = sub_exists(t)
 
-while temp == False:
-    print("that is not valid, try again")
-    start = input()
-    if end.isnumeric() and len(end) == 10:
-        temp = True
-print("from the subreddit , enter the end date for the "
+print("from the subreddit " + t + " , enter the start date for the "
                                   "posts you would like to download, in unix timestamp format")
 start = input()
 temp = False
@@ -69,6 +91,18 @@ while temp == False:
     print("that is not valid, try again")
     start = input()
     if start.isnumeric() and len(start) == 10:
+        temp = True
+print("from the subreddit " + t + ", enter the end date for the "
+                                  "posts you would like to download, in unix timestamp format")
+end = input()
+temp = False
+if end.isnumeric() and len(end) == 10 :
+    temp = True
+
+while temp == False:
+    print("that is not valid, try again")
+    end = input()
+    if end.isnumeric() and len(end) == 10:
         temp = True
 
 
@@ -84,22 +118,24 @@ while temp == False:
     if cc.isnumeric():
         temp = True
 
+print(t)
+sub= t
+#before and after dates
 
-start = str(start)
-print(start, end)
-end = str(end)
-sub = 'askreddit'
-# start and end dates
-#start = "1590971471"  # October 1st
-#end = "1590364800"  # January 1st
-print(start, end)
-query = "Screenshot"
+
+before = end
+after = start
+#after = "1590364800"  #January 1st
+
+#before = "1590971471" #October 1st
+query = ""
 subCount = 0
 subStats = {}
 
-data = getPushshiftData(query, end, start, sub)
+
+data = getPushshiftData(query, after, before, sub)
 # Will run until all posts have been gathered
-# from the 'end' date up until start date
+# from the 'after' date up until before date
 while len(data) > 0:
     for submission in data:
         collectSubData(submission, 10)
@@ -107,8 +143,8 @@ while len(data) > 0:
     # Calls getPushshiftData() with the created date of the last submission
     print(len(data))
     print(str(datetime.datetime.fromtimestamp(data[-1]['created_utc'])))
-    end = data[-1]['created_utc']
-    data = getPushshiftData(query, end, start, sub)
+    after = data[-1]['created_utc']
+    data = getPushshiftData(query, after, before, sub)
 
 print(len(data))
 
@@ -119,11 +155,12 @@ print("Last entry is:")
 print(list(subStats.values())[-1][0][1] + " created: " + str(list(subStats.values())[-1][0][5]))
 
 
+
 def updateSubs_file():
     upload_count = 0
     location = "downloaded/"
-    print("input filename of submission file, please add .csv")
-    filename = input()
+    print("input filename of submission file")
+    filename = input() + ".csv"
     file = location + filename
     with open(file, 'w', newline='', encoding='utf-8') as file:
         a = csv.writer(file, delimiter=',')
